06cd3be871a52d1d1a3233c38c5ae91001d4d81c
Sasha <kremlink0@gmail.com>
2020-08-20 14:30:21 +0300

    cartograph process

--- js/modules/cartogr/data.js
+++ js/modules/cartogr/data.js
@@ -2,10 +2,12 @@ export const data={
  wait:300000,
  grid:[10,6],
  active:[0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,1,1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1],
+ win:{startIndex:11,picsAmount:35,wait:1500},
  seq:[35,14,22,43,24,12,33,25,17,36,28,16,18,26,13,34,42,44,23,27,32,45,11],
  start:['F',5],
  events:{
-  'go':'.pulse-btn'
+  'go':'.pulse-btn',
+  'gItem':'.g-item'
  },
  view:{
   el:'.ov-wrap.cartogr',
@@ -16,6 +18,7 @@ export const data={
   doneCls:'done',
   shownCls:'shown',
   mTmpl:'#cartogr-m-template',
+  nextTmpl:'#cartogr-next-template',
   item:{
    tmpl:'#cartogr-item-template',
    activeCls:'active',
--- js/modules/cartogr/view.js
+++ js/modules/cartogr/view.js
@@ -3,6 +3,7 @@ import {BaseIntView} from '../baseInteractive/view.js';
 
 let events={};
 events[`click ${data.events.go}`]='go';
+events[`click ${data.events.gItem}`]='gItem';
 
 export let CartogrView=BaseIntView.extend({
  el:data.view.el,
@@ -10,8 +11,11 @@ export let CartogrView=BaseIntView.extend({
  done:false,
  itemTemplate:_.template($(data.view.item.tmpl).html()),
  mTemplate:_.template($(data.view.mTmpl).html()),
+ nextTemplate:_.template($(data.view.nextTmpl).html()),
  $grid:null,
  $next:null,
+ $previews:null,
+ current:0,
  $gridItems:null,
  initialize:function(){
   BaseIntView.prototype.initialize.apply(this,[{
@@ -25,24 +29,57 @@ export let CartogrView=BaseIntView.extend({
  },
  render:function(){
   let s='',
-      m='';
+      m='',
+  winIndex;
 
   for(let i=0;i<data.grid[1];i++)
   {
    for(let j=0;j<data.grid[0];j++)
    {
-    s+=this.itemTemplate({active:data.active[i*data.grid[0]+j],width:100/data.grid[0],height:100/data.grid[1],left:j*100/data.grid[0],top:i*100/data.grid[1]});
+    winIndex=i*data.grid[0]+j-data.win.startIndex;
+    s+=this.itemTemplate({active:data.active[i*data.grid[0]+j],winIndex:winIndex<0||winIndex>=data.win.picsAmount?-1:winIndex,width:100/data.grid[0],height:100/data.grid[1],left:j*100/data.grid[0],top:i*100/data.grid[1]});
     m+=this.mTemplate({text:j?String.fromCharCode(data.start[0].charCodeAt(0)+j)+(data.start[1]+i):'',left:j*100/data.grid[0],top:i*100/data.grid[1]});
    }
   }
 
-  this.$gridItems=$(s);
-  this.$grid.append(this.$gridItems);
+  this.$grid.append(this.$gridItems=$(s));
   this.$grid.append($(m));
+  s='';
+  for(let i=0;i<data.seq.length;i++)
+   s+=$.trim(this.nextTemplate({index:i}));
+  this.$next.append(this.$previews=$(s));
+  this.$previews.eq(this.current).addClass(data.view.item.activeCls);
  },
- win:function(){
-  this.$el.addClass(data.view.doneCls);
-  this.done=true;
+ gItem:function(e){
+  let item=$(e.currentTarget),
+   index=item.index();
+
+  //this.$el.addClass(data.view.doneCls);
+  //this.done=true;
+
+  if(item.hasClass(data.view.item.activeCls))
+  {
+   if(index===data.seq[this.current])
+   {
+    item.removeClass(data.view.item.badCls).addClass(data.view.item.goodCls+' '+data.view.doneCls);
+    this.$previews.eq(this.current++).removeClass(data.view.item.activeCls);
+    this.$previews.eq(this.current).addClass(data.view.item.activeCls);
+    if(this.current===data.seq.length)
+    {
+     setTimeout(()=>{
+      this.$el.addClass(data.view.doneCls);
+      this.done=true;
+     },data.win.wait);
+    }
+   }else
+   {
+    if(!item.hasClass(data.view.doneCls))
+    {
+     item.removeClass(data.view.item.badCls);
+     setTimeout(()=>item.addClass(data.view.item.badCls),100);
+    }
+   }
+  }
  },
  go:function(){
   if(this.done)
