97c9cbb8863c8ad4d01447139196dee549f5ce77
Sasha <kremlink0@gmail.com>
2020-10-26 01:43:14 +0300

    board fix

--- js/modules/boardMgr/view.js
+++ js/modules/boardMgr/view.js
@@ -4,7 +4,6 @@ let data=app.configure({board:dat}).board;
 
 export let BoardMgr=Backbone.View.extend({
  data:{name:'',what:'',points:0},
- points:{},
  initialize:function(){
   this.listenTo(app.get('aggregator'),'board:name',this.name);
   this.listenTo(app.get('aggregator'),'board:score',this.score);
@@ -18,15 +17,16 @@ export let BoardMgr=Backbone.View.extend({
   this.req();
  },
  sum:function(){
-   return Object.values(this.points).reduce((ac,cur)=>ac+cur);
+  let ls=JSON.parse(localStorage.getItem(app.get('ls')));
+
+  return Object.values(ls.points).reduce((ac,cur)=>ac+cur);
  },
  score:function({what,points}){
   let ls=JSON.parse(localStorage.getItem(app.get('ls')));
 
   this.data.what=what;
   this.data.points=points;
-  this.points[what]=points;
-  ls.points=this.points;
+  ls.points[what]=points;
   localStorage.setItem(app.get('ls'),JSON.stringify(ls));
   this.req();
  }
